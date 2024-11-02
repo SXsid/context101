5c6a13215a94ffa8f9b0dff0c13b1ca09c8613a2
sudhanshu20204 <sidshekhar2024@gmail.com>
2024-11-02 08:51:25 +0530

    reduc --init

---
+++ Redux101/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <ol style={{textAlign:"left"}}>
+      <li>store:keep all the state here</li>
+      <li>reducer:funciton to magnge state</li>
+      <li>useSelect</li>
+      <li>uesDispatch:</li>
+      
+    </ol>
+  )
+}
+
+export default App
